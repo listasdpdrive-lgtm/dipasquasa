b0c9af9bac78325db512cfda14787af914913127
listasdpdrive-lgtm <listasdpdrive@gmail.com>
2025-10-03 08:31:24 -0300

    Update route.ts

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -110,7 +110,7 @@ export async function POST(req: NextRequest) {
 
     await transporter.sendMail({
       from: `"${process.env.MAIL_FROM_NAME}" <${process.env.SMTP_USER}>`,
-      to: "dipasquarepuestos@gmail.com",
+      to: process.env.EMAIL_RECEIVER,
       subject: `Nuevo mensaje de ${name}`,
       replyTo: email,
       text: `Nombre: ${name}\nEmail: ${email}\n\nMensaje:\n${message}`,
