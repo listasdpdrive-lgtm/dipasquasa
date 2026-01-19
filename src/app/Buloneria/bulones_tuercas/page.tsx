6017fba3335bf8c5c53c4e430b3d40fb91f17356
listasdpdrive-lgtm <listasdpdrive@gmail.com>
2026-01-19 12:54:11 -0300

    Add files via upload

---
+++ src/app/Buloneria/bulones_tuercas/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import Link from "next/link"
+import Image from "next/image"
+import Sidebar from "@/components/Sidebar"
+import { link } from "fs"
+
+export default function Accesorios() {
+  const cuadros = [
+    {
+      img: "/buloneria/bulon_rueda.png",
+      titulo: "Bulones de rueda",
+      pdf: "/catalogos/BULONERIA/BULONES DE RUEDA.pdf",
+    },
+    {
+      img: "/buloneria/tuercas.png",
+      titulo: "Tuercas de rueda",
+      pdf: "/catalogos/BULONERIA/TUERCAS DE RUEDA.pdf",
+    },
+    { img: "/buloneria/bulones.jpg", 
+      titulo: "Buloneria General",
+       link: "/Buloneria/buloneria_general" },
+  ]
+
+  return (
+    <div className="flex min-h-screen relative">
+    
+
+      {/* FONDO */}
+      <div
+        className="fixed inset-0 bg-cover bg-center -z-10"
+        style={{ backgroundImage: "url('/fondo_web.png')" }}
+      />
+      <div className="fixed inset-0 bg-gradient-to-br from-black/80 via-black/60 to-black/90 -z-10" />
+
+      <Sidebar />
+
+      {/* CONTENIDO */}
+      <main className="flex-1 px-4 mt-20 md:px-10 pt-24">
+        {/* CONTENEDOR CENTRADO */}
+        <div className="max-w-6xl mx-auto">
+          {/* GRID */}
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-8">
+            {cuadros.map((c, i) => {
+              const Card = (
+                <div className="bg-white rounded-2xl shadow-xl overflow-hidden hover:scale-105 transition cursor-pointer">
+                  <Image
+                    src={c.img}
+                    alt={c.titulo}
+                    width={500}
+                    height={500}
+                    className="w-full h-39 md:h-70 object-cover"
+                  />
+                  <h3 className="p-4 bg-red-600 text-center font-bold text-sm md:text-base">
+                    {c.titulo}
+                  </h3>
+                </div>
+              )
+
+              return c.pdf ? (
+                <a
+                  key={i}
+                  href={c.pdf}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {Card}
+                </a>
+              ) : (
+                <Link key={i} href={c.link!}>
+                  {Card}
+                </Link>
+              )
+            })}
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/app/Buloneria/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import Link from "next/link"
+import Image from "next/image"
+import Sidebar from "@/components/Sidebar"
+
+export default function Accesorios() {
+
+  const cuadros = [
+    { img: "/buloneria/alemites.png", titulo: "Alemites", pdf: "/catalogos/BULONERIA/ALEMITES.pdf" },
+    { img: "/buloneria/chavetas.png", titulo: "Chavetas", pdf:"/catalogos/BULONERIA/CHAVETAS.pdf" },
+    
+    { img: "/buloneria/buloneriaG.png", titulo: "Buloneria general", link: "/Buloneria/bulones_tuercas" },
+  ]
+
+  return (
+    <div className="flex min-h-screen">
+
+      {/* SIDEBAR */}
+      <Sidebar />
+
+      {/* CONTENIDO */}
+      <main className="flex-1 relative">
+
+
+        {/* FONDOS */}
+        <div
+          className="fixed inset-0 bg-cover bg-center -z-10"
+          style={{ backgroundImage: "url('/fondo_web.png')" }}
+        />
+        <div className="fixed inset-0 bg-black/70 -z-10" />
+
+        {/* CUADROS */}
+        <div className="min-h-screen px-4 py-24 md:p-20 flex justify-center">
+          <div className="w-full max-w-6xl">
+
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-10">
+              {cuadros.map((c, i) => {
+
+                const Wrapper = c.link ? Link : "a"
+                const wrapperProps = c.link
+                  ? { href: c.link }
+                  : { href: c.pdf, target: "_blank", rel: "noopener noreferrer" }
+
+                return (
+                  <Wrapper key={i} {...wrapperProps}>
+                    <div
+                      className="bg-white rounded-xl shadow-lg overflow-hidden
+                                 transition hover:scale-105 active:scale-95 cursor-pointer"
+                    >
+                      <Image
+                        src={c.img}
+                        alt={c.titulo}
+                        width={600}
+                        height={600}
+                        className="w-full h-44 sm:h-52 md:h-69 object-cover"
+                      />
+                      <h3 className="p-4 bg-red-600 text-center font-bold text-base sm:text-lg">
+                        {c.titulo}
+                      </h3>
+                    </div>
+                  </Wrapper>
+                )
+              })}
+            </div>
+
+          </div>
+        </div>
+
+      </main>
+    </div>
+  )
+}
