6478bce2e143816612c6688ab5c93797ca804210
listasdpdrive-lgtm <listasdpdrive@gmail.com>
2026-02-03 12:03:42 -0300

    Update footer link from Facebook to WhatsApp

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -31,7 +31,7 @@ export function Footer() {
             </p>
             <div className="flex gap-4">
               <motion.a
-                href="https://www.facebook.com/profile.php?id=100087217878036"
+                href="https://web.whatsapp.com/"
                 target="_blank"
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.1 }}
