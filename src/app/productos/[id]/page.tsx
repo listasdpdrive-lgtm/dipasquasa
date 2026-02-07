dcbb5abdff809114f60e1b4a6d5cee207c180e8e
listasdpdrive-lgtm <listasdpdrive@gmail.com>
2026-02-07 08:47:55 -0300

    descripcion productos

--- src/app/productos/[id]/page.tsx
+++ src/app/productos/[id]/page.tsx
@@ -468,8 +468,8 @@ const eliminarImagen = async (url: string) => {
                 <h1 className="text-3xl font-bold">{producto.name}</h1>
                 <p className="text-2xl text-green-600">${producto.price}</p>
                 <p className="whitespace-pre-line">
-  {producto.description}
-</p>
+                 {producto.description}
+                </p>
 
 
                 {/* TABLAS PÚBLICAS */}
