f534314534988ae9ddaa26d8a8b05b34383eb250
lucasaravena495-dotcom <lucasaravena495@gmail.com>
2026-07-29 17:27:04 -0300

    vigia

--- src/app/casa_repuestos/page.tsx
+++ src/app/casa_repuestos/page.tsx
@@ -31,7 +31,7 @@ export default function CatalogosPage() {
     { img: "/puntaeje/punta_eje.png", titulo: "TREN DELANTERO Y PUNTA DE EJE", link: "/punta_eje" },
     { img: "/PESADOS-JPG/pesadito.png", titulo: "PESADOS", link: "/Pesados" },
     { img: "/varios.png", titulo: "ACCESORIOS VARIOS", link: "/Varios" },
-    { img: "/image-cuadros/eevigis.png", titulo: "VIGIA", pdf: "/catalogos/Vigia 30-07-26.pdf" },
+    { img: "/image-cuadros/eevigis.png", titulo: "VIGIA", pdf: "/catalogos/Vigia 31-07-26.pdf" },
   ]
 
   useEffect(() => {7
